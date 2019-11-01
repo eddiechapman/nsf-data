36bdb5bc1419f4dc302a373a6ec47de9c32c3e4a
eddiechapman <eddie.chapman4@gmail.com>
2019-11-01 17:49:42 -0500

    Create setup script.

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="nsf-data",
+    author="Eddie Chapman",
+    author_email="edward.chapman@marquette.edu",
+    description="Download and parse NSF award data.",
+    long_description=long_description,
+    url="https://github.com/eddiechapman/nsf-data",
+    packages=setuptools.find_packages(),
+    scripts=['scripts/download.py', 'scripts/filter.py'],
+    requires=[
+        'beautifulsoup4',
+        'lxml',
+        'requests',
+    ]
+)
+
