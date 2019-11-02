c6b5f70ca92c2d350fcec08e292bf813b3fd8180
eddiechapman <eddie.chapman4@gmail.com>
2019-11-02 14:07:33 -0500

    Update script paths.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     url="https://github.com/eddiechapman/nsf-data",
     packages=setuptools.find_packages(),
-    scripts=['scripts/download.py', 'scripts/filter.py'],
+    scripts=['scripts/download.py', 'scripts/filter_awards.py'],
     requires=[
         'nltk',
         'beautifulsoup4',
