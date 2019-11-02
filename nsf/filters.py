a211c68e6c0ef35b05fd8d5875227cef3f1bcbf2
eddiechapman <eddie.chapman4@gmail.com>
2019-11-02 13:38:34 -0500

    Supply default arguments for filter_date

--- nsf/filters.py
+++ nsf/filters.py
@@ -1,7 +1,14 @@
+import datetime
+
 from nsf.keyword import match_keywords
 
 
 def filter_date(award, start, end):
+    if start is None:
+        start = datetime.date(1950, 1, 1)
+    if end is None:
+        end = datetime.date.today()
+
     return award.effective >= start and award.effective <= end
 
 
