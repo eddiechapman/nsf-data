2a95359dd10fbef5cf645d9b8d20100a88129cff
eddiechapman <eddie.chapman4@gmail.com>
2019-11-04 18:42:14 -0600

    Change keyword search to re. Move to filters.py.

--- nsf/filters.py
+++ nsf/filters.py
@@ -1,6 +1,8 @@
+import collections
 import datetime
+import re
 
-from nsf.keyword import match_keywords
+from nsf import KEYWORD_DATA
 
 
 def filter_date(award, start, end):
@@ -16,5 +18,30 @@ def filter_directorate(award, directorate):
     return award.directorate == directorate
 
 
-def filter_abstract(award, keywords, n=None):
-    return bool(match_keywords(award.abstract, keywords, n))
+def filter_abstract(award):
+    if not award.abstract:
+        return False
+    try:
+        hits = match_keywords(award.abstract)
+    except SearchTermsNotFound:
+        return False
+
+    return True
+
+
+def match_keywords(keywords, text, n=None):
+    hits = collections.Counter()
+
+    with open(KEYWORD_DATA / 'keywords.txt', 'r') as f:
+        for search_term in f:
+            for match in re.finditer(search_term, text, re.I):
+                hits.update({search_term: 1})
+
+    if not hits:
+        raise SearchTermsNotFound
+
+    return hits
+
+
+class SearchTermsNotFound(Exception):
+    pass
